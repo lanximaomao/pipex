32fcb391a6b84dc22f9b57a8100b9afd99c5f8ff
Lin <lsun@student.hive.fi>
2023-02-16 19:21:16 +0200

    working and pass all test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 17:29:59 by lsun              #+#    #+#             */
-/*   Updated: 2023/02/16 18:15:08 by lsun             ###   ########.fr       */
+/*   Updated: 2023/02/16 19:18:13 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ int	main(int argc, char **argv, char **env)
 	}
 	pipex = malloc(sizeof(t_pipex));
 	if (!pipex)
-		exit(EXIT_FAILURE);
+		exit(1);
 	pipex->env = env;
 	pipex_init(pipex, argv);
 	if (get_path(pipex) == 1)
-		exit(EXIT_FAILURE);
+		exit(1);
 	if (get_pipe(pipex) == 1)
-		exit(EXIT_FAILURE);
+		exit(1);
 	free_all(pipex);
 	return (0);
 }
